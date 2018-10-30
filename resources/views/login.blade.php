af3740a569267fa2e3fcdef0cfb6f2924cd5f05e
Vu Nguyen <vun@sfu.ca>
2018-10-29 20:40:09 -0700

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
   <style type="text/css">
 
   .modal-header, h4, .close {
@@ -20,6 +21,7 @@
   }
 
 </style>
+
 </head>
 <body>
 <div class="container">
