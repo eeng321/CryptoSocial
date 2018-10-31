38f2527c168b47dabd8b2ef481c01a6c19e43f8f
Junho Kim <jnhkm311@gmail.com>
2018-10-30 19:59:51 -0700

    Update users search page

--- resources/views/userspage.blade.php
+++ resources/views/userspage.blade.php
@@ -7,10 +7,23 @@
 @section('content')
     @parent()
     <div class="container p-3">
-        <form class="form-inline ml-auto">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search User" aria-label="Search User">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+            <form class="form-inline" method="get" action="#">
+                <input class="form-control mr-sm-2 ml-auto" type="search" placeholder="User to search" aria-label="User to search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            
+            <div class="card">
+                
+                <div class="card-body">
+                    <h2 style="display:inline;">Yeetlers</h2>
+                    {{-- Replace this with total users returned --}}
+                    <span style="display:inline;">- 0 users returned</span>
+                    
+                    <hr>
+
+                    {{-- This is where you display the rows of users --}}
+                </div>
+            </div>
     </div>
 @endsection()
 
