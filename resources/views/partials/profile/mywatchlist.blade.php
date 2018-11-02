ad6a296dd0af7fc7ce420bef8c56b1de2bed3a41
eeng <eeng@sfu.ca>
2018-11-02 02:32:09 -0700

    WIP profile page, mywatchlist, remove sidepanel and put into profile.blade, update app.scss

---
+++ resources/views/partials/profile/mywatchlist.blade.php
@@ -0,0 +1,32 @@
+<div class="card">
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Market Cap</th>
+            <th scope="col">Price</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Bitcoin</td>
+            <td>$110,609,408,006</td>
+            <td>$1337</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Urmumcoin</td>
+            <td>6969</td>
+            <td>69</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>XRP</td>
+            <td>9001</td>
+            <td>0.69</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,9 +3,41 @@
 @section('title', 'Profile')
 
 @section('navbar')
+    @parent()
+ 
+@endsection
 
 @section('content')
-    @include('partials.profile.sidepanel')
+
+        <div class="wrapper">
+            <!-- Sidebar -->
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <p>Username</p>
+                </div>
+        
+                <ul class="list-unstyled components">
+                    <li>
+                        <a href="#">News Update</a>
+                    </li>
+                    <li>
+                        <a href="#">Watchlist</a>
+                    </li>
+                    <li>
+                        <a href="#">Trades</a>
+                    </li>
+                </ul>
+        
+            </nav>
+            
+            <div id="content">
+                <div class="container-fluid">
+                    @include('partials.profile.mywatchlist')
+                </div>
+            </div>
+        </div>
+       
+
 @endsection
 
 @section('footer')
