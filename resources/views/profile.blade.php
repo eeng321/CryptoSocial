8363ea559f2214adb4b1b3658e57e255b6a30dd6
eeng <eeng@sfu.ca>
2018-11-04 05:22:30 -0800

    add profile css into app.scss, add user content on profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,31 +11,31 @@
         <div class="wrapper">
             <!-- Sidebar -->
             <aside>
-                <div id="sidebar" class="nav-collapse ">
+                <div id="sidebar" class="nav-collapse " tabindex="5000" style="overflow:hidden; outline:none;">
                   <!-- sidebar menu start-->
                   <ul class="sidebar-menu" id="nav-accordion">
                     <p class="centered"><a href="/profile"><img src="{{"img/pika.png"}}" class="img-circle" width="80"></a></p>
                     <h5 class="centered">Boku no Pika</h5>
                     <li>
-                        <a href="/profile">
+                        <a class="dcjq-parent" href="javascript:;">
                             <i class="fa fa-dashboard"></i>
                             <span>Dashboard</span>
                         </a>
                     </li>
                     <li>
-                        <a href="/profile">
+                        <a class="dcjq-parent" href="javascript:;">
                             <i class="fa fa-rocket"></i>
                             <span>Trades</span>
                         </a>
                     </li>
                     <li>
-                        <a href="/profile">
+                        <a class="dcjq-parent" href="javascript:;">
                             <i class="fa fa-sticky-note"></i>
                             <span>Watchlist</span>
                         </a>
                     </li>
                     <li class="sub-menu">
-                        <a href="javascript:;">
+                        <a class="dcjq-parent" href="javascript:;">
                             <i class="fa fa-comments-o"></i>
                             <span>Chat Room</span>
                         </a>
@@ -52,16 +52,40 @@
 
             <!-- Main Content -->
             <section id="content">
-                <div class="row">
-                    <div class="col-md-16">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title text-center">Watchlist</h5>
-                                @include('partials.profile.mywatchlist')
-                            </div>
+                <div class="border-head">
+                    <h3 class="text-center">MENTOR SUGGESTIONS</h3>
+                </div>
+                <div class="row mt content-panel">
+                    <div class="col-md-4 profile-text mt mb centered">
+                        <div class="right-divider hidden-sm hidden-xs">
+                        <h4>1922</h4>
+                        <h6>FOLLOWERS</h6>
+                        <h4>290</h4>
+                        <h6>FOLLOWING</h6>
+                        <h4>$ 13,980</h4>
+                        <h6>MONTHLY EARNINGS</h6>
                         </div>
                     </div>
-                </div>    
+                    <!-- /col-md-4 -->
+                    <div class="col-md-4 profile-text">
+                        <h3>Squirtle</h3>
+                        <h6>Squirtle Squad Leader</h6>
+                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC.</p>
+                        <br>
+                        <p><button class="btn btn-theme"><i class="fa fa-envelope"></i> Send Message</button></p>
+                    </div>
+                    <!-- /col-md-4 -->
+                    <div class="col-md-4 centered">
+                        <div class="profile-pic">
+                        <p><img src="{{"img/squirtle.png"}}" class="img-circle" width="150"></p>
+                        <p>
+                            <button class="btn btn-theme"><i class="fa fa-check"></i> Follow</button>
+                            <button class="btn btn-theme02">Add</button>
+                        </p>
+                        </div>
+                    </div>
+                    <!-- /col-md-4 -->
+                </div>   
             </section>
         </div>
        
