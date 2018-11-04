1a9101eb02f9b37bd606bafd1fa742c02d280eb6
Osborn Cheng <osbornc@sfu.ca>
2018-11-04 11:17:58 -0800

    Added middleware to get current auth user data into views as $userData  - added a middleware group user_set  - any pages/routes that need access can be added in web.php  - if user isn't authed $userData will be null

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,14 @@ Route::get('watchlist', function() {
 Route::get('trades', function() {
     return view('trades');
 });
+//testing sharing user_data across views
+// gets current auth()->user() shares for each view in $userData 
+Route::group(['middleware' => ['user_set']], function() {
+    //routes that use ^ middleware defined in \App\Http\Kernel.php
+    Route::get('userspage', 'SearchController@search');
+});
 
-Route::get('userspage', 'SearchController@search');
+
 
 //array to register many resource controllers when we add more in the future
 Route::resources([
