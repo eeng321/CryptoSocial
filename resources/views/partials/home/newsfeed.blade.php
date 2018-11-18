80d3cfbb2cd91a067ca3286ce53ad317febf1e37
Junho Kim <jnhkm311@gmail.com>
2018-11-18 14:58:13 -0800

    Change newsfeed and open links in new tab

--- resources/views/partials/home/newsfeed.blade.php
+++ resources/views/partials/home/newsfeed.blade.php
@@ -1,23 +1,30 @@
+<?php 
+    function displayArticle($article) {
+        echo "<div class='row'>";
+        echo "<div class='col blogShort'>";
+        echo "<em><strong>".$article['author']."</strong></em>";
+        echo "<img class='rounded float-left article-thumbnail' src=".$article['urlToImage']." alt='Card image cap'>";
+        echo "<article><p>".$article['description']."</p></article>";
+        echo  "<a class='btn btn-secondary article-link' target='_blank' href=".$article['url']." role='button'>Read More</a>";
+        echo "</div>";        
+        echo "</div>";
+    }
+?>
+
 <div class="container">
     <div class="card">
         <div class="card-body">
             <h1 class="display-4 text-center">News Feed</h1>
             @if(!empty($news))
+                <div class='container-fluid'>
                 @foreach(array_chunk($news['articles'], 3) as $articles)
-                <div class='row'>
-                @foreach($articles as $article)
-                @php
-                        echo "<div class='col-md-4'>";
-                        echo "<img class='card-img-thumb' src=".$article['urlToImage']." alt='Card image cap'>";
-                        echo "<h2 class= 'card-title'>".$article['author']."</h2>";
-                        echo "<p class= 'card-text news-card'>".$article['description']."</p>";
-                        echo  "<a class='btn btn-secondary' href=".$article['url']." role='button'>View details &raquo;</a>";
-                        echo "</div>";
+                    @foreach($articles as $article)
+                    @php
+                        displayArticle($article);
                     @endphp
                     @endforeach
-                </div>
-                <hr>
                 @endforeach
+                </div>
             @else
                 <h2 class='card-title text-center'>No news to display</h2>
             @endif
