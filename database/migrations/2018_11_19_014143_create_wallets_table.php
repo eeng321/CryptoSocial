c66d87b2a11ae702b757975dcd910e39856a351f
swanhell <stephen_wanhella@sfu.ca>
2018-11-18 17:52:56 -0800

    add wallets table migration

---
+++ database/migrations/2018_11_19_014143_create_wallets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('coin');
+            $table->decimal('amount', 10, 10);
+            $table->primary(array('user_id', 'coin'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
